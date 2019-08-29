da07741514cf0d2b5f8125ea9cf569cd34ea4e5e
Ashley <ashley@znet.me>
2019-08-29 14:23:46 -0500

    Updates

---
+++ precache-manifest.f89570e7138cb9ebb4d56fa58bbdba6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4fd22b82409d3684ef9f7dcccdc515",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99d74e7c3bb110b569aa",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "38ef49855e5fdd3408bb",
+    "url": "./static/js/2.1ca181ad.chunk.js"
+  },
+  {
+    "revision": "99d74e7c3bb110b569aa",
+    "url": "./static/js/main.e41a9d70.chunk.js"
+  },
+  {
+    "revision": "a9a6afcc89d6f9073f54",
+    "url": "./static/js/runtime~main.d0784117.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/generate-a-sim/precache-manifest.126167164e07f904fe3e551a3b8de9f9.js"
+  "./precache-manifest.f89570e7138cb9ebb4d56fa58bbdba6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/generate-a-sim/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
