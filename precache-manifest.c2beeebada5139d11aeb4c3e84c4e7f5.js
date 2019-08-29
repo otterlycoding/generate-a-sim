f243b7f34d3a5893d9422da465d56d765065d120
Ashley <ashley@znet.me>
2019-08-29 14:25:11 -0500

    Updates

---
+++ precache-manifest.c2beeebada5139d11aeb4c3e84c4e7f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c319a0268ecdd8f506f5a7fea0a47c0",
+    "url": "otterlycoding/index.html"
+  },
+  {
+    "revision": "9217d704051703a4f896",
+    "url": "otterlycoding/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "38ef49855e5fdd3408bb",
+    "url": "otterlycoding/static/js/2.1ca181ad.chunk.js"
+  },
+  {
+    "revision": "9217d704051703a4f896",
+    "url": "otterlycoding/static/js/main.1a71a0db.chunk.js"
+  },
+  {
+    "revision": "8d842ff1272d71f25f4f",
+    "url": "otterlycoding/static/js/runtime~main.aaa21b93.js"
+  }
+]);
